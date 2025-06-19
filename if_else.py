c0d6b95077c7dfb939d85edd8b2779eafb3e6eb0
Ritesh Chavan <155073367+Chavan-Ritesh@users.noreply.github.com>
2025-06-19 20:29:06 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+if a<b :
+	print("a is smaller than b")
+elif a==b :
+	print("a equals to b")
+	
+else  :
+	print(" a is greater than b")
